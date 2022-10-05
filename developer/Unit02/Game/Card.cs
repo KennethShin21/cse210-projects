b416aab43893ddc704ce67dab0008a9741521497
Kenneth Benting <kenneth.benting@gmail.com>
2022-10-05 10:13:39 +0800

    additional updates for project HiLo

---
+++ developer/Unit02/Game/Card.cs
@@ -0,0 +1,42 @@
+using System;
+
+
+namespace Unit02.Game
+{
+    /// <summary>
+    /// Individual cards that are represented as a number from 1 to 13.
+    /// 
+    /// The responsibility of the card is to hold the value and to see if it is higher
+    /// or lower than the guessed value by the user.
+    ///
+    /// The player starts with 300 points. 
+    /// </summary>
+    public class Card
+    {
+        public int _cardValue = 0;
+        public int _cardNextValue = 0;
+
+        /// <summary>
+        /// Constructs a new instance of Card.
+        /// </summary>
+
+        public Card()
+        {
+        }
+        
+        /// <summary>
+        /// Generates a random card from 1 to 13. Then, generates another random card
+        /// from 1 to 13. And store the value.
+        ///  
+        /// </summary>
+
+        public void Roll()
+        {
+            Random random = new Random();
+            _cardValue = random.Next(1, 13);
+            Console.WriteLine(_cardValue);
+            _cardNextValue = random.Next(1, 13);
+        }
+    }
+
+}
---
+++ developer/Unit02/README.md
@@ -0,0 +1,22 @@
+
+## HiLo Game
+Hilo is a game in which the player guesses if the next card drawn by the dealer will be higher or lower than the previous one. Points are won or lost based on whether or not the player guessed correctly.
+
+## Project Structure
+---
+The project files and folders are organized as follows:
+```
+root                    (project root folder)
++-- Game                (source code folder)
++-- Program.cs          (program entry point)    
++-- README.md           (general info)
++-- Unit02.csproj       (dotnet project file)
+```
+
+## Required Technologies
+---
+* dotnet 6.0
+
+## Authors
+---
+*Kenneth Benting
